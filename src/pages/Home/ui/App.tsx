48510b1fa93c900de20a62f837c00cbebfc1db0c
yaroslav <boliachevets.ya@gmail.com>
2026-04-19 23:24:29 +0100

    fix: app query path

--- src/pages/Home/ui/App.tsx
+++ src/pages/Home/ui/App.tsx
@@ -19,7 +19,7 @@ export const App = () => {
 
 	const { data } = useQuery({
 		queryKey: ['filters'],
-		queryFn: () => fetch('/filterData.json').then(res => res.json())
+		queryFn: () => fetch('./filterData.json').then(res => res.json())
 	})
 
 	return (
